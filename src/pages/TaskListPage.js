4c42461197906090b75795d22a2ffb12dc7a30e4
Joshua Mabina <mabinajoshua@gmail.com>
2020-06-02 04:39:59 +0300

    refactor: use hooks

--- src/pages/TaskListPage.js
+++ src/pages/TaskListPage.js
@@ -1,54 +1,52 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Task from '../components/Task';
 import InputForm from '../components/InputForm';
 
-class TaskListPage extends Component {
-  state = {
-    tasks: [
-      { description: 'Code', time: new Date().toString(), isComplete: false },
-      { description: 'Eat', time: new Date().toString(), isComplete: false },
-      { description: 'Sleep', time: new Date().toString(), isComplete: false }
-    ]
-  };
-  submitTask = event => {
+function TaskListPage() {
+  const [tasks, setTasks] = useState([
+    { description: 'Code', time: new Date().toString(), isComplete: false },
+    { description: 'Eat', time: new Date().toString(), isComplete: false },
+    { description: 'Sleep', time: new Date().toString(), isComplete: false }
+  ]);
+
+  const submitTask = event => {
     const task = {
-      id: this.state.tasks.length + 1,
+      id: tasks.length + 1,
       description: event.target[0].value,
       time: event.target[1].value,
       isComplete: false
     };
 
-    const taskList = [task].concat(this.state.tasks);
+    const taskList = [task].concat(tasks);
 
-    this.setState({ tasks: taskList });
+    setTasks(taskList);
   };
 
-  onComplete = index => {
-    const newTasks = [].concat(this.state.tasks);
+  const onComplete = index => {
+    const newTasks = [].concat(tasks);
     newTasks[index].isComplete = true;
 
-    this.setState({ tasks: newTasks });
+    setTasks(newTasks);
   };
 
-  render() {
-    return (
-      <div className="main">
-        <p>This is our list</p>
+  return (
+    <div className="main">
+      <p>This is our list</p>
 
-        <InputForm addTask={this.submitTask} />
+      <InputForm addTask={submitTask} />
 
-        {this.state.tasks.map((task, index) => (
-          <Task
-            index={index}
-            description={task.description}
-            time={task.time}
-            complete={task.isComplete}
-            onComplete={this.onComplete}
-          />
-        ))}
-      </div>
-    );
-  }
+      {tasks.map((task, index) => (
+        <Task
+          key={index}
+          index={index}
+          description={task.description}
+          time={task.time}
+          complete={task.isComplete}
+          onComplete={onComplete}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default TaskListPage;
